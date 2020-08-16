021ebe291623e7ee8cd78c88fdf4746c313118a9
osidekyle <hawkinskyle67@gmail.com>
2020-08-16 02:04:20 -0700

    basically finished css of opening piece, just need to hide header after sliding out

--- Voice-Imager/voice-imager/src/App.js
+++ Voice-Imager/voice-imager/src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import Header from "./components/Header"
+import {bounceInDown,bounceOutUp} from 'react-animations'
+import styled, { keyframes } from 'styled-components';
 import './App.css';
 
 function App() {
+
+
+  const Bounce = styled.div`animation: 2s ${keyframes`${bounceInDown}`} `;
+  const BounceOutUp = styled.div`animation: 1s ${keyframes`${bounceOutUp}`}`;
+  
+
+  const [clicked, setClicked]= useState(false);
+
+  const handleClick=(e)=>{
+    e.preventDefault();
+    setClicked(true);
+    console.log("clicked");
+    console.log(clicked);
+  }
+
+  
+
+
+
+  const hidden={
+    display:"none"
+  }
   return (
     <div className="App">
-      <Header/>
+      {clicked ? <BounceOutUp><Header style="display:none" /></BounceOutUp> :<Bounce><Header handleClick={handleClick}/></Bounce> }
+     
     </div>
   );
 }
--- Voice-Imager/voice-imager/src/components/Header.js
+++ Voice-Imager/voice-imager/src/components/Header.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React,{useState} from 'react';
 import bootstrap from "../../../node_modules/bootstrap/dist/css/bootstrap.css"
+import { CSSTransition } from 'react-transition-group';
+import "../App.css"
 
-const Header = () => {
+const Header = ({handleClick}) => {
+
+
+    const [emptyButton, setEmptyButton]=useState(false);
+
+
+
+
     return ( 
         <div className="col-xs-12 m-3">
             <div className="row justify-content-center m-5">
@@ -10,9 +19,13 @@ const Header = () => {
            <div className="row justify-content-center m-5">
            <h4 className="display-4">Create Image Designs with just voice commands</h4>
            </div>
+           
            <div className="row justify-content-center m-5">
-           <btn className="btn btn-primary btn-lg p-3">Get Started</btn>
+          
+           <button className="p-4 goButton" onClick={handleClick} >Get Started</button>
+   
            </div>
+           
         </div>
      );
 }
