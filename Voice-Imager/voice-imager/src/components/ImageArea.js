c326bc86007ec0ebbd258dc947ce18573cd6eb4f
osidekyle <hawkinskyle67@gmail.com>
2020-08-19 01:41:28 -0700

    got pictures loading in with voice

--- Voice-Imager/voice-imager/src/components/ImageArea.js
+++ Voice-Imager/voice-imager/src/components/ImageArea.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import bootstrap from "../../../node_modules/bootstrap/dist/css/bootstrap.css"
+import axios from "../../../node_modules/axios/dist/axios"
+
+
+
+
 const ImageArea = () => {
     const [islistening, setIsListening] =useState(false)
     const [words, setWords]=useState("");
-
+    const [links, setLinks]=useState([]);
 
 
     const SpeechRecognition = window.speechRecognition || window.webkitSpeechRecognition;   
@@ -17,27 +22,73 @@ const ImageArea = () => {
         recognition.start();
 
     };
-    
 
     recognition.onresult=(event)=>{
         
-        console.log(event.results[event.results.length-1][0].transcript);
         
-        setWords(event.results[event.results.length-1][0].transcript);
+        
+        parseWords(event.results[event.results.length-1][0].transcript);
+        
+       
     }
     useEffect(()=>{
         recognition.start();
     },[])
 
+    useEffect(()=>{
+        if(words!=""){
+            const headers= {
+                "headers":{
+                "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
+                "x-rapidapi-key": "d5dedf0503mshd5df683c1996f5ap154385jsn6d5327764ac2"
+                }
+            }
+            
+            axios.get(`https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?autoCorrect=false&pageNumber=1&pageSize=10&q=${words}&safeSearch=false`,headers)
+        .then(response => {
+            
+            updateLinks(response.data.value[0].thumbnail)
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+    },[words])
 
+   
+   
+    const parseWords=(sentence)=>{
+        let finals="";
+        sentence=sentence.split(" ")
+        for(let word of sentence)
+        {
+            if(word!="draw" && word!="a")
+            {
+                finals+=word+" "
+            }
+        }
+        setWords(finals)
+        
+    }
 
+    const updateLinks=(link)=>{
+        /*
+        let temp=links;
+        temp[word.slice(0,-1)]=link;
+        console.log(temp["dog"])
+        */
+        setLinks((oldArray)=>[...oldArray,link]);
+    }
     return ( 
         <div className="row justify-content-center">
             <div>
-                <h1  className="display-1 mt-5">{words}</h1>
+                {links.map((link)=>(
+                 <img  src={link}/>
+                ))}
             </div>
         </div>
      );
 }
+
  
 export default ImageArea;
