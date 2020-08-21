2bcdb6c919d358567cb42cd59bc3cd67e330fb02
osidekyle <hawkinskyle67@gmail.com>
2020-08-20 19:56:42 -0700

    made it so the images can be moved with voice

--- Voice-Imager/voice-imager/src/components/ImageArea.js
+++ Voice-Imager/voice-imager/src/components/ImageArea.js
@@ -9,7 +9,6 @@ const ImageArea = () => {
     const [islistening, setIsListening] =useState(false)
     const [words, setWords]=useState("");
     const [links, setLinks]=useState([]);
-    const imageContainer=useRef(null)
 
     const SpeechRecognition = window.speechRecognition || window.webkitSpeechRecognition;   
      let listening=false;
@@ -28,7 +27,7 @@ const ImageArea = () => {
         
         
         parseWords(event.results[event.results.length-1][0].transcript);
-        
+        console.log(event.results[event.results.length-1][0].transcript)
        
     }
     useEffect(()=>{
@@ -37,17 +36,20 @@ const ImageArea = () => {
 
     useEffect(()=>{
         if(words!=""){
+            let tempwords=words.trim().split(" ");
+            console.log(tempwords)
+            if(tempwords[0]=="draw"){
             const headers= {
                 "headers":{
                 "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
-                "x-rapidapi-key": "secret"
+                "x-rapidapi-key": "d5dedf0503mshd5df683c1996f5ap154385jsn6d5327764ac2"
                 }
             }
             
-            axios.get(`https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?autoCorrect=false&pageNumber=1&pageSize=10&q=${words}clipart&safeSearch=false`,headers)
+            axios.get(`https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?autoCorrect=false&pageNumber=1&pageSize=10&q=${tempwords[1]}%20clipart&safeSearch=false`,headers)
         .then(response => {
             let temp={}
-            temp['name']=words
+            temp['name']=tempwords[1]
             temp["url"]=response.data.value[0].thumbnail;
             temp["left"]=0;
             temp["top"]=0;
@@ -57,18 +59,52 @@ const ImageArea = () => {
         .catch(err => {
             console.log(err);
         });
+    }
 
-        console.log(imageContainer.childNodes)
+    else if(tempwords[0]=="move"){
+        console.log("got move")
+        for (let i=0;i<links.length;i++){
+            if(links[i]["name"]==tempwords[1]){
+                console.log("got name")
+                if(tempwords[2]=="down"){
+                    console.log("got down")
+                   let tempLinks=links;
+                   tempLinks[i]["top"]+=100;
+                   setLinks(tempLinks)
+                }
+                if(tempwords[2]=="up"){
+                    console.log("got down")
+                   let tempLinks=links;
+                   tempLinks[i]["top"]-=100;
+                   setLinks(tempLinks)
+                }
+                if(tempwords[2]=="left"){
+                    console.log("got down")
+                   let tempLinks=links;
+                   tempLinks[i]["left"]-=100;
+                   setLinks(tempLinks)
+                }
+                if(tempwords[2]=="right"){
+                    console.log("got down")
+                   let tempLinks=links;
+                   tempLinks[i]["left"]+=100;
+                   setLinks(tempLinks)
+                }
+            }
+        }
+    }
+
+    setWords("")
     }
     },[words])
-
+    
     
     const parseWords=(sentence)=>{
         let finals="";
         sentence=sentence.split(" ")
         for(let word of sentence)
         {
-            if(word!="draw" && word!="a")
+            if(word!="a" && word!="the")
             {
                 finals+=word+" "
             }
